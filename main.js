4eea54eb64abb8276abe88530f4b5aee2d19a2b9
Karlina Skiba <karolina.joanna.skiba@gmail.com>
2020-06-30 00:23:33 +0200

    scrollIntoVeiw functiom

--- main.js
+++ main.js
@@ -2,9 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
 
     const rootElement = document.querySelector('#root');
-    const section = document.querySelectorAll('section');
+    const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isTrottled = false;
 
     document.addEventListener('mousewheel', (e) => {
-        console.log(e.wheelDelta)
+        if (isTrottled) return;
+        isTrottled = true;
+        setTimeout(function () {
+            isTrottled = false;
+        }, 1000)
+        const direction = e.wheelDelta < 0 ? 1 : -1;
+
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if (firstSection) return
+        }
+        currentSectionIndex = currentSectionIndex + direction;
+        console.log(currentSectionIndex);
+
+        sections[currentSectionIndex].scrollIntoView({ behavior: "smooth", block: "start", })
+
     })
 });
