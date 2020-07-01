62dcac515e5415debd210427308d32953c0c3504
Karlina Skiba <karolina.joanna.skiba@gmail.com>
2020-07-02 00:21:50 +0200

    add navigation to scroller

--- Scroller.js
+++ Scroller.js
@@ -7,7 +7,9 @@ class Scroller {
         this.currentSectionIndex = Math.max(currentSectionIndex, 0);
         this.currentSectionIndex = 0;
         this.isTrottled = false;
-        this.scroll = this.scroll.bind(this)
+        this.scroll = this.scroll.bind(this); // <=> arrow function
+
+        this.drawNavigation();
     }
 
     isScrollIntoView(el) {
@@ -48,7 +50,43 @@ class Scroller {
 
     }
     scrollToCurrentSection = () => {
+        this.selectAtiveNavItem();
         this.sections[this.currentSectionIndex].scrollIntoView({ behavior: "smooth" })
+    }
+    drawNavigation = () => {
+        this.navigationContainer = document.createElement('aside');
+        this.navigationContainer.setAttribute('class', 'scroller__navigation');
+        const list = document.createElement('ul');
+
+        this.sections.forEach((section, index) => {
+            const listItem = document.createElement('li');
+            listItem.addEventListener('click', () => {
+                this.currentSectionIndex = index;
+                this.scrollToCurrentSection();
+            })
+            list.appendChild(listItem);
+            this.navigationContainer.appendChild(list);
+
+            document.body.appendChild(this.navigationContainer);
+
+            this.selectAtiveNavItem();
+
+        })
+    }
+
+    selectAtiveNavItem = () => {
+        if (this.navigationContainer) {
+            const naviItem = this.navigationContainer.querySelectorAll('li');
+
+            naviItem.forEach((item, index) => {
+                if (index === this.currentSectionIndex) {
+                    item.classList.add('active')
+                } else {
+                    item.classList.remove('active');
+                }
+
+            })
+        }
     }
 
 }
